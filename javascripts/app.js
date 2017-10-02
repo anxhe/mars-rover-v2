0bc3f73cbf830ce5ddf04f62833f485a97c4c0b3
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-10-02 17:15:07 +0200

    --amend

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,10 +1,9 @@
 
+var grid = new Grid();
+var oportunity = new Rover("Oportunity", "🚘 ", [0,0]);
+var curiosity = new Rover("Curiosity", "🚍", [0,9]);
+
 window.onload = function() {
-  var grid = new Grid();
-
-  var oportunity = new Rover("Oportunity", "🚘 ", [0,0]);
-  var curiosity = new Rover("Curiosity", "🚍", [0,9]);
-
   grid.addObject(oportunity);
   grid.addObject(curiosity);
 
